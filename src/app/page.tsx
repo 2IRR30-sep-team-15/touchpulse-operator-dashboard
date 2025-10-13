d1ab30419e44710a7ee6ce5ee70de1d4ab939b54
broskoy <mihnea.buzoiu@gmail.com>
2025-10-14 00:47:49 +0200

    merged login page too

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-"use client";
-
-import Sidebar from "@/components/sidebar";
-import Map from "@/components/map";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen w-screen">
-      {/* Sidebar takes 1/4 of the width */}
-      <div className="w-1/4">
-        <Sidebar />
-      </div>
-
-      {/* Map takes the remaining 3/4 */}
-      <div className="w-3/4">
-        <Map />
-      </div>
-    </div>
-  );
+  redirect("/dashboard");
 }
