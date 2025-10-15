009932ea1693e1087210c6299527a37d307f8440
bszel <147652796+bszel@users.noreply.github.com>
2025-10-15 13:27:02 +0200

    added user routes
    -display routes
    -display location with marker
    -select user on map

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -2,7 +2,8 @@
 
 
 import * as React from 'react';
-import { Map, Marker } from 'react-map-gl/maplibre';
+import { Map, NavigationControl, Marker, Source, Layer, useMap } from 'react-map-gl/maplibre';
+import type { LineLayerSpecification } from 'react-map-gl/maplibre'
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, SelectGroup } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
@@ -22,15 +23,23 @@ export default function InteractiveMap() {
 
     const [selectedStyle, setSelectedStyle] = React.useState<'dark' | 'light' | 'satellite' >('dark');
 
+    const users = ['user1', 'user2', 'user3'];
+    const [selectedUser, setSelectedUser] = React.useState(undefined);
+
     return (
         <div className="relative w-full h-full">
-            <MyMap mapStyle={styles[selectedStyle]} />
+            <MyMap 
+                mapStyle={styles[selectedStyle]}
+                users={users}
+                selectedUser={selectedUser}
+                setSelectedUser={setSelectedUser}
+            />
 
-            <div className="absolute bottom-4 right-4 z-10 w-[14%]">
+            <div className="absolute bottom-12 right-4">
                 <SelectLayer selectedStyle={selectedStyle} setSelectedStyle={setSelectedStyle} />
             </div>
 
-            <div className="absolute top-4 right-4 z-10">
+            <div className="absolute top-4 left-4">
                 <SearchBar />
             </div>
         </div>
@@ -38,8 +47,8 @@ export default function InteractiveMap() {
 }
 
 
-function MyMap({ mapStyle }: { mapStyle: string}) {
-
+function MyMap({ mapStyle, users, selectedUser, setSelectedUser }:
+    { mapStyle: string; users: Array<string>; selectedUser?: String; setSelectedUser: (value: any) => void }) {
     return (
         <Map
             initialViewState={{
@@ -49,13 +58,13 @@ function MyMap({ mapStyle }: { mapStyle: string}) {
             }}
             style={{width: '100%', height: '100%'}}
             mapStyle={mapStyle}
+            onClick={() => setSelectedUser(undefined)}
         >
-            <Marker longitude={5.476} latitude={51.439} anchor="bottom" />
-            <Marker longitude={5.481} latitude={51.435} anchor="bottom" />
+            <DisplayUsers users={users} selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <NavigationControl />
         </Map>
     );
 }
-
 
 function SelectLayer({ selectedStyle, setSelectedStyle }: { selectedStyle: string; setSelectedStyle: (value: any) => void }) {
     return (
@@ -74,14 +83,93 @@ function SelectLayer({ selectedStyle, setSelectedStyle }: { selectedStyle: strin
     );
 }
 
-
 function SearchBar() {
     return (
-        <div className="flex w-full max-w-sm items-center gap-4">
-            <Input className="bg-white" placeholder="Search address here..." />
+        <div className="flex w-full max-w-sm items-center gap-2">
+            <Input className="bg-white" type="email" placeholder="Search address here..." />
             <Button type="submit" variant="outline">
                 Search
             </Button>
         </div>
+    );
+}
+
+function DisplayUsers({ users, selectedUser, setSelectedUser }: { users: Array<string>; selectedUser?: String; setSelectedUser: (value: any) => void }) {
+    // display all users if none is selected
+    if (!selectedUser) {
+        return (
+            <>{users.map((user, index) => (
+                <React.Fragment key={index}>
+                    <UserLocation user={user} setSelectedUser={setSelectedUser} />
+                    <UserRoute user={user} />
+                </React.Fragment>
+            ))}</>
+        )
+    }
+
+    // display only the selected user
+    return (
+        <>
+            <UserLocation user={selectedUser} setSelectedUser={setSelectedUser} />
+            <UserRoute user={selectedUser} />
+        </>
+    )
+}
+
+function UserLocation({ user, setSelectedUser }: { user: String; setSelectedUser: (value: any) => void }) {
+    const {current: map} = useMap();
+    
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch(`/data/location_${user}.json`).then(res => res.json()).then(setData);
+    });
+
+    if (!data) return null;
+
+    const coordinates = data["coordinates"];
+    const longitude = coordinates[0];
+    const latitude = coordinates[1];
+
+    return (
+        <Marker
+            longitude={longitude}
+            latitude={latitude}
+            onClick={(e) => {
+                e.originalEvent.stopPropagation();
+                setSelectedUser(user);
+                map?.easeTo({center: coordinates, zoom: 14});
+            }}
+        />
+    );
+}
+
+function UserRoute({ user }: { user: String }) {
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch(`/data/route_${user}.geojson`).then(res => res.json()).then(setData);
+    });
+
+    if (!data) return null;
+
+    const routeLayer : LineLayerSpecification = {
+        id: `route_${user}`,
+        type: 'line',
+        source: 'maplibre',
+        paint: {
+            'line-color': '#01b4af',
+            'line-width': 4
+        },
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        }
+    }
+    
+    return (
+        <Source id={`route_${user}`} type="geojson" data={data}>
+            <Layer {...routeLayer} />
+        </Source>
     );
 }
