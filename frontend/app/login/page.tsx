af622029f5082424b01d651233fed70c2ff52122
broskoy <mihnea.buzoiu@gmail.com>
2025-10-20 18:38:33 +0200

    login page looks good now

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -7,15 +7,17 @@ const LoginTemplate = () => {
   return (
     <div className="container relative grid h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-gray-900 p-10 text-primary-foreground dark:border-r lg:flex">
-        <div className="relative z-20 flex items-center text-lg font-medium">
+        <div className="relative z-20 flex items-center text-white text-lg font-medium">
           <MapPin className="mr-2 h-8 w-8" />
           Pathway Dashboard
         </div>
       </div>
-      <div className="lg:p-8 bg-background">
+      <div className="lg:p-8 bg-gray-200 h-full flex items-center justify-center">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+            <h1 className="text-2xl text-black font-semibold tracking-tight">
+              Login
+            </h1>
             <p className="text-sm text-muted-foreground">
               Log in to your account
             </p>
--- frontend/components/login/index.tsx
+++ frontend/components/login/index.tsx
@@ -13,8 +13,8 @@ export default function LoginForm({ className, ...props }: LoginFormProps) {
   return (
     <div className={cn('grid gap-6', className)} {...props}>
       <form onSubmit={onFormSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
+        <div className="grid gap-4">
+          <div className="grid gap-4">
             <Label className="sr-only" htmlFor="username">
               Username
             </Label>
@@ -26,23 +26,25 @@ export default function LoginForm({ className, ...props }: LoginFormProps) {
               autoComplete="username"
               autoCorrect="off"
               disabled={isLoading}
+              className="border-gray-300"
             />
             <Label className="sr-only" htmlFor="password">
               Password
             </Label>
             <Input
               id="password"
-              placeholder="********"
+              placeholder="password"
               type="password"
               autoCapitalize="none"
               autoComplete="password"
               autoCorrect="off"
               disabled={isLoading}
+              className="border-gray-300"
             />
           </div>
           <Button
             disabled={isLoading}
-            className="bg-[#E17100] hover:bg-[#C86400] transition-colors"
+            className="bg-[#E17100] text-white hover:bg-[#C86400] transition-colors"
           >
             {isLoading && <Loader className="mr-2 h-4 w-4 animate-spin" />}
             Enter
