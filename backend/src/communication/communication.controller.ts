32f85afb294e5b26d6cd3930d2e757ace63a8c53
Chachi <w.jiaqi004@gmail.com>
2025-10-20 12:54:33 +0200

    backend: add(livekit getToken)
    Generate a livekit token for a given room and username

--- backend/src/communication/communication.controller.ts
+++ backend/src/communication/communication.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CommunicationService } from './communication.service';
 
 @Controller('communication')
 export class CommunicationController {
-  constructor(private communicationService: CommunicationService) {}
-  @Post('token')
-  getToken() {
-    return this.communicationService.getToken();
+  constructor(private readonly communicationService: CommunicationService) {}
+
+  @Get('token')
+  getToken(@Query('room') room: string, @Query('username') username: string) {
+    if (!room) {
+      throw new BadRequestException('Missing "room" query parameter');
+    }
+
+    if (!username) {
+      throw new BadRequestException('Missing "username" query parameter');
+    }
+
+    const token = this.communicationService.getToken(room, username);
+    return { token };
   }
 }
--- backend/src/communication/communication.service.ts
+++ backend/src/communication/communication.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { AccessToken } from 'livekit-server-sdk';
 
 @Injectable()
 export class CommunicationService {
-  getToken() {
-    return 'test-token';
+  private apiKey: string;
+  private apiSecret: string;
+
+  constructor() {
+    if (!process.env.LIVEKIT_API_KEY || !process.env.LIVEVKIT_API_SECRET) {
+      throw new InternalServerErrorException(
+        'LiveKit API key/secret not configured',
+      );
+    }
+    this.apiKey = process.env.LIVEKIT_API_KEY;
+    this.apiSecret = process.env.LIVEVKIT_API_SECRET;
+  }
+
+  getToken(roomName: string, participantName: string) {
+    const at = new AccessToken(this.apiKey, this.apiSecret, {
+      identity: participantName,
+      ttl: '10m',
+    });
+    at.addGrant({
+      roomJoin: true,
+      canPublish: true,
+      canSubscribe: true,
+      room: roomName,
+    });
+    return at.toJwt();
   }
 }
