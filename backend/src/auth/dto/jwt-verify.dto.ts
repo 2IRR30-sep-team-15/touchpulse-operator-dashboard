05261f45499a0b3106d93f59de1aa4a8933e6560
SimoSimo24 <simeon.kroko@gmail.com~>
2025-10-24 09:23:05 +0200

    Reimplemented swagger to backend

--- backend/src/auth/dto/jwt-verify.dto.ts
+++ backend/src/auth/dto/jwt-verify.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class JwtVerifyDto {
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...', description: 'JWT to verify' })
   token: string;
 }
--- backend/src/operators/operators.controller.ts
+++ backend/src/operators/operators.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiTags('operators')
+@ApiBearerAuth('access-token')
 @Controller('operators')
 export class OperatorsController {
   @UseGuards(AuthGuard)
